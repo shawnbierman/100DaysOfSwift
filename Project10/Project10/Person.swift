e93be7e0b736260ee18c545bbb40c376825b28a7
Shawn Bierman <shawnbierman@icloud.com>
2019-03-17 12:30:07 -0500

    Project 10, Part II. Several new ideas covered here and a revist of a few previous ideas.

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Shawn Bierman on 3/17/19.
+//  Copyright © 2019 Shawn Bierman. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
