3e241b4077b0341454e77193366b667e7a3bef42
Shawn Bierman <shawnbierman@icloud.com>
2019-04-26 18:40:02 -0500

    Project 19, Part One

---
+++ Milestone6/Milestone6/GameScene.swift
@@ -0,0 +1,36 @@
+//
+//  GameScene.swift
+//  Milestone6
+//
+//  Created by Shawn Bierman on 4/21/19.
+//  Copyright © 2019 Shawn Bierman. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    @objc fileprivate func createTarget() {
+        
+    }
+}
---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Shawn Bierman on 4/26/19.
+//  Copyright © 2019 Shawn Bierman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
