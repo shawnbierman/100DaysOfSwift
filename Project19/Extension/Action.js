d41b9bb9fb2129ff9a6d5dd33c5288bf0fdca4a8
Shawn Bierman <shawnbierman@icloud.com>
2019-04-28 08:33:01 -0500

    Project 19, Part Three

--- Project19/Extension/Action.js
+++ Project19/Extension/Action.js
@@ -3,7 +3,7 @@ var Action = function() { }
 Action.prototype = {
     
 run: function(parameters) {
-    parameters.completionFunction({"URL": document.URL, "title": document.title});
+    parameters.completionFunction({"URL": document.URL, "title": document.title, "lastModified": document.lastModified});
 },
     
 finalize: function(parameters) {
--- Project19/Extension/ActionViewController.swift
+++ Project19/Extension/ActionViewController.swift
@@ -15,16 +15,21 @@ class ActionViewController: UIViewController {
     
     var pageTitle = ""
     var pageURL = ""
+    var lastModified = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // navigationBar buttons
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(scripts))
         
+        // handle keyboard
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         
+        //
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
@@ -33,6 +38,7 @@ class ActionViewController: UIViewController {
                     
                     self?.pageTitle = javaScriptValues["title"] as? String ?? ""
                     self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+                    self?.lastModified = javaScriptValues["lastModified"] as? String ?? ""
                     
                     DispatchQueue.main.async {
                         self?.title = self?.pageTitle
@@ -44,13 +50,47 @@ class ActionViewController: UIViewController {
 
     @IBAction func done() {
         let item = NSExtensionItem()
-        let argument: NSDictionary = ["customJavaScript": script.text]
+        let argument: NSDictionary = ["customJavaScript": script.text!]
         let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
         let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
         item.attachments = [customJavaScript]
         extensionContext?.completeRequest(returningItems: [item])
     }
 
+    @objc func scripts() {
+        
+        let ac = UIAlertController(title: "Actions", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Last Modified Date", style: .default, handler: { [weak self] (action) in
+            let item = NSExtensionItem()
+            let argument: NSDictionary = ["customJavaScript": "alert(document.lastModified);"]
+            let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+            let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+            item.attachments = [customJavaScript]
+            self?.extensionContext?.completeRequest(returningItems: [item])
+        }))
+        
+        ac.addAction(UIAlertAction(title: "Title", style: .default, handler: { [weak self] (action) in
+            let item = NSExtensionItem()
+            let argument: NSDictionary = ["customJavaScript": "alert(document.title);"]
+            let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+            let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+            item.attachments = [customJavaScript]
+            self?.extensionContext?.completeRequest(returningItems: [item])
+        }))
+        
+        ac.addAction(UIAlertAction(title: "Location", style: .default, handler: { [weak self] (action) in
+            let item = NSExtensionItem()
+            let argument: NSDictionary = ["customJavaScript": "alert(document.documentURI);"]
+            let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+            let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+            item.attachments = [customJavaScript]
+            self?.extensionContext?.completeRequest(returningItems: [item])
+        }))
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(ac, animated: true)
+    }
+    
     @objc func adjustForKeyboard(notifcation: Notification) {
         guard let keyboardValue = notifcation.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
         let keyboardScreenEndFrame = keyboardValue.cgRectValue
