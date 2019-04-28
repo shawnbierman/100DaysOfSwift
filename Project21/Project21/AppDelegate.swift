c6f90d6adb120ba666c78e46e30b18d40be30000
Shawn Bierman <shawnbierman@icloud.com>
2019-04-28 14:46:59 -0500

    Project 21, Part One

---
+++ Project21/Project21/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Project21
+//
+//  Created by Shawn Bierman on 4/28/19.
+//  Copyright © 2019 Shawn Bierman. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
