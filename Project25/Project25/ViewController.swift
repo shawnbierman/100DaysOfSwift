a901483e1df25753202c3286df2fe086a0254816
Shawn Bierman <shawnbierman@icloud.com>
2019-05-25 13:56:28 -0500

    Project 25, Part Two (w/ Challenges) [Day 84/100]

--- Project25/Project25/ViewController.swift
+++ Project25/Project25/ViewController.swift
@@ -22,11 +22,36 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         super.viewDidLoad()
 
         title = "Selfie Share"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
+
+        setupNavbar()
 
         mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
         mcSession?.delegate = self
+    }
+
+    fileprivate func setupNavbar() {
+
+        // right buttons
+        let infoButton = UIButton(type: .infoLight)
+            infoButton.addTarget(self, action: #selector(showConnections), for: .touchUpInside)
+        let importPictureButton = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+        let showConnectionsButton = UIBarButtonItem(customView: infoButton)
+        let rightBarButtonItems = [importPictureButton, showConnectionsButton]
+
+        // left buttons
+        let connectionButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
+        let chatButton = UIButton(type: .contactAdd)
+            chatButton.addTarget(self, action: #selector(showChatDialog), for: .touchUpInside)
+        let startChatButton = UIBarButtonItem(customView: chatButton)
+        let leftBarButtonItems = [connectionButton, startChatButton]
+
+        navigationItem.rightBarButtonItems = rightBarButtonItems
+        navigationItem.leftBarButtonItems = leftBarButtonItems
+    }
+
+    deinit {
+        mcSession?.disconnect()
+        mcAdvertiserAssistant?.stop()
     }
 
     func startHosting(action: UIAlertAction) {
@@ -38,27 +63,65 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     func joinSession(action: UIAlertAction) {
         guard let mcSession = mcSession else { return }
         let mcBrowser = MCBrowserViewController(serviceType: projectIdentifier, session: mcSession)
-        mcBrowser.delegate = self
+            mcBrowser.delegate = self
         present(mcBrowser, animated: true)
+    }
+
+    // -- MARK: Challenge #3
+    @objc func showConnections() {
+        guard let connections = mcSession else { return }
+        let peers = connections.connectedPeers.map { $0.displayName }
+        let ac = UIAlertController(title: "Connections \(peers.count)", message: peers.joined(separator: "\n"), preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true)
     }
 
     @objc func importPicture() {
         let picker = UIImagePickerController()
-        picker.allowsEditing = true
-        picker.delegate = self
+            picker.allowsEditing = true
+            picker.delegate = self
         present(picker, animated: true)
     }
 
     @objc func showConnectionPrompt() {
         let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
-        ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
-        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+            ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
+            ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(ac, animated: true)
+    }
+
+    // -- MARK: Challenge #2
+    @objc func showChatDialog() {
+        let ac = UIAlertController(title: "Send Message", message: nil, preferredStyle: .alert)
+            ac.addTextField(configurationHandler: nil)
+            ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            ac.addAction(UIAlertAction(title: "Send", style: .default, handler: { [weak self, weak ac] _ in
+                guard let text = ac?.textFields?[0].text else { return }
+                self?.sendChatText(text: text)
+            }))
+        present(ac, animated: true)
+    }
+
+    // -- MARK: Challenge #2
+    fileprivate func sendChatText(text: String) {
+        guard let mcSession = mcSession else { return }
+
+        if mcSession.connectedPeers.count > 0 {
+            let textData = Data(text.utf8)
+            do {
+                try mcSession.send(textData, toPeers: mcSession.connectedPeers, with: .reliable)
+            } catch {
+                let ac = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                present(ac, animated: true)
+            }
+        }
     }
 }
 
 extension ViewController {
+
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
@@ -68,12 +131,12 @@ extension ViewController {
         if let imageView = cell.viewWithTag(1000) as? UIImageView {
             imageView.image = images[indexPath.item]
         }
-
         return cell
     }
 }
 
 extension ViewController {
+
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let image = info[.editedImage] as? UIImage else { return }
         dismiss(animated: true)
@@ -89,7 +152,7 @@ extension ViewController {
                     try mcSession.send(imageData, toPeers: mcSession.connectedPeers, with: .reliable)
                 } catch {
                     let ac = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
-                    ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
                     present(ac, animated: true)
                 }
             }
@@ -98,14 +161,12 @@ extension ViewController {
 }
 
 extension ViewController {
-    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
-    }
 
-    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
-    }
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) { }
 
-    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
-    }
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) { }
+
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) { }
 
     func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
         dismiss(animated: true)
@@ -126,16 +187,30 @@ extension ViewController {
         case .notConnected:
             print("notConnected: \(peerID.displayName)")
 
+            // -- MARK: Challenge #1
+            let message = "\(peerID.displayName) has disconnected."
+            let ac = UIAlertController(title: "Disconnection", message: message, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            DispatchQueue.main.async { [weak self] in
+                self?.present(ac, animated: true)
+            }
+
         @unknown default:
             print("Unknown state received: \(peerID.displayName)")
         }
     }
 
     func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
         DispatchQueue.main.async { [weak self] in
             if let image = UIImage(data: data) {
                 self?.images.insert(image, at: 0)
                 self?.collectionView.reloadData()
+            } else {
+                let ac = UIAlertController(title: "Message", message: String(data: data, encoding: .utf8), preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self?.present(ac, animated: true)
+                print("received> \(String(describing: String(data: data, encoding: .utf8)))")
             }
         }
     }
